5a348bdbba1bd2f8e76d29200ebb33bcd21bc531
donohoe14 <donohoe14@aol.com>
2024-03-09 20:20:07 +0000

    Update events-api.spec.js

--- tests/events-api.spec.js
+++ tests/events-api.spec.js
@@ -1,5 +1,5 @@
 // @ts-check
-import { test, expect } from '@playwright/test';
+ import { test, expect } from '@playwright/test';
 
 test('event creation', async ({ request }) => {
   const testTitle = 'Test event';
